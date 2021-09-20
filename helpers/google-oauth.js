a1eb790e2909f32e317d06f3d6088f8191bad0ce
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-09-20 17:39:40 +0100

    Create google oauth helpers

---
+++ helpers/google-oauth.js
@@ -0,0 +1,33 @@
+const { default:axios } = require('axios')
+
+const getGoogleTokens = async (code) => {
+    const tokensUrl = 'https://oauth2.googleapis.com/token'
+    const data = new URLSearchParams()
+    data.append('client_id', process.env.google_client_id)
+    data.append('client_secret', process.env.google_client_secret)
+    data.append('code', code)
+    data.append('grant_type', 'authorization_code')
+    data.append('redirect_uri', 'http://localhost:8080/oauth/google/callback')
+
+    const response = await axios.post(tokensUrl, data, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+
+    return response.data
+}
+
+const getGoogleAccountInfo = async (token) => {
+    const infoUrl = new URL('https://www.googleapis.com/oauth2/v1/userinfo')
+    infoUrl.searchParams.append('alt', 'json')
+    infoUrl.searchParams.append('access_token', token)
+
+    const response = await axios.get(infoUrl.href)
+    return response.data
+}
+
+module.exports = {
+    getGoogleTokens,
+    getGoogleAccountInfo,
+}
