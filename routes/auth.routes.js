c20a7db05624a7201ab16d7405134b87ba156684
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-09-29 18:38:59 +0100

    Fix conflict

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { login, signup, loginDiscord, resetPass } = require('../controllers/auth.controller');
 const { authenticatedOnly } = require('../middlewares/auth.middleware');
+
 const router = express.Router();
 
 router.post('/login', login);
@@ -10,6 +11,5 @@ router.post('/signup', signup);
 router.post('/loginDiscord', loginDiscord);
 
 router.post('/resetPass', authenticatedOnly, resetPass);
-
 
 module.exports = router;
