057636da5d668693240ef5b41234444af187d523
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-09-29 17:13:43 +0100

    Fix conflict

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const expressformidable = require('express-formidable-v2')
 
@@ -11,21 +10,3 @@ router.put('/profile', controllers.updateProfile)
 router.post('/upload-avatar', expressformidable(), controllers.uploadProfileImage)
 
 module.exports = router
-=======
-const express = require('express');
-const { show, findById, create, update, deleteUser} = require('../controllers/user.controller');
-
-const router = express.Router();
-
-router.get('/show', show);
-
-router.get('/:id', findById);
-
-router.post('/create', create);
-
-router.put('/update/:id', update);
-
-router.delete('/delete/:id', deleteUser)
-
-module.exports = router;
->>>>>>> 1c5370e (fix bug)
