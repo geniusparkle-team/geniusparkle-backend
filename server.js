f7f34fe8f0f4cfde420611fac0d0f21aacc6adc2
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-12 05:44:09 +0100

    Organize Server file

--- server.js
+++ server.js
@@ -1,70 +1,54 @@
-const express = require('express');
-const cors = require('cors');
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
 
-require('dotenv').config();
+require('dotenv').config()
 
-// create express app
-const app = express();
+const authRouter = require('./routes/auth.routes')
+const oauthRouter = require('./routes/oauth.routes')
+const mailRouter = require('./routes/mail.routes')
+const userRouter = require('./routes/user.routes')
+const videosRouter = require('./routes/videos.routes')
+const { verrifyGoogleTokens } = require('./middlewares/google-oauth.middleware')
+const { authentication, authenticatedOnly } = require('./middlewares/auth.middleware')
 
-// Setup server port
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
-// Trust proxy to know if http or https is being used
-app.enable('trust proxy')
 
-// CORS
 const corsMiddleware = cors({
     origin: '*',
-    allowedHeaders: ['Content-Type', 'Authorization', 'Authentication']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Authentication'],
 })
 
-app.use(corsMiddleware);
-app.options('*', corsMiddleware);
+app.set('emails_views', path.resolve(__dirname, 'EmailTemplates'))
+app.enable('trust proxy')
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
 app.use(express.json())
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // error handling logic
-    console.error(err.stack);
-    res.status(500).json({ msg: "something wrong" });
+    console.error(err.stack)
+    res.status(500).json({ msg: 'something wrong' })
+})
 
-});
+app.use(corsMiddleware)
+app.options('*', corsMiddleware)
 
 // Authentication Checker
-const { authentication, authenticatedOnly } = require('./middlewares/auth.middleware')
 app.use(authentication)
 
-// define a simple route
 app.get('/', (req, res) => {
-    res.json({ "message": "genius-park-api" });
-});
+    res.json({ message: 'genius-park-api' })
+})
 
-// Handles oauth callbacks
-const oauthRouter = require('./routes/oauth.routes')
 app.use('/oauth/', oauthRouter)
-
-// auth routes
-const authRouter = require('./routes/auth.routes');
-app.use('/api/auth', authRouter);
-
-// mail routes
-const mailRouter = require('./routes/mail.routes');
-app.use('/api/mail', mailRouter);
-
-// Videos Api Routes
-const videosRouter = require('./routes/videos.routes');
-const { verrifyGoogleTokens } = require('./middlewares/google-oauth.middleware');
+app.use('/api/auth', authRouter)
+app.use('/api/mail', mailRouter)
 app.use('/api/videos', authenticatedOnly, verrifyGoogleTokens, videosRouter)
-
-// User data routes
-const userRouter = require('./routes/user.routes')
 app.use('/api/user', authenticatedOnly, verrifyGoogleTokens, userRouter)
 
-// listen for requests
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+    console.log(`Server is listening on port ${port}`)
+})
